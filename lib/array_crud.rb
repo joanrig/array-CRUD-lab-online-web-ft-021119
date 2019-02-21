fe2f09658f79066cfab9aa589d8362489329ff49
Joan Lyness <joanrigdon@gmail.com>
2019-02-21 06:01:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array.new = []  
+  array.new = []
 end
 
 def add_element_to_end_of_array(array, element)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
